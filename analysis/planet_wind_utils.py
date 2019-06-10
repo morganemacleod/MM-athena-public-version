27c6bd367cf1a76f099e6d32a9d53080229ba438
Morgan <morganmacleod@morgan-laptop.local>
2019-06-10 14:52:27 +0200

    added new utilities to form rays to the stellar surface and interpolate values along them

--- analysis/planet_wind_utils.py
+++ analysis/planet_wind_utils.py
@@ -165,3 +165,62 @@ def pos_secondary(orb,t):
     y2 = np.interp(t,orb['time'],orb['y'])
     z2 = np.interp(t,orb['time'],orb['z'])
     return x2,y2,z2
+
+
+### Ray tracing
+from scipy.interpolate import RegularGridInterpolator
+
+def get_interp_function(d,var):
+    dph = np.gradient(d['x3v'])[0]
+    x3v = np.append(d['x3v'][0]-dph,d['x3v'])
+    x3v = np.append(x3v,x3v[-1]+dph)
+    
+    var_data = np.append([d[var][-1]],d[var],axis=0)
+    var_data = np.append(var_data,[var_data[0]],axis=0)
+    
+    var_interp = RegularGridInterpolator((x3v,d['x2v'],d['x1v']),var_data,bounds_error=False)
+    return var_interp
+
+def cart_to_polar(x,y,z):
+    r = np.sqrt(x**2 + y**2 +z**2)
+    th = np.arccos(z/r)
+    phi = np.arctan2(y,x)
+    return phi,th,r
+
+def get_ray(YZoffset=(0,0),star_pos=(-1.e11,0,0),length=1.e11,phase=0.0,inclination=0.0,rstar=7.e10,npoints=100):
+    """ returns a ray in the intrinsic x,y,z coordinates of the mesh.
+    After rotations, The viewer is along the -X axis, and the stellar surface is projected on the Y-Z plane"""
+    xstar,ystar,zstar=star_pos
+    Ystar,Zstar = YZoffset
+    Xstar = -np.sqrt(rstar**2-Ystar**2-Zstar**2)
+    
+    # define dir vector (has length 1.0)
+    dz = np.sin(inclination)
+    dy = -np.cos(inclination)*np.sin(phase)
+    dx = -np.cos(inclination)*np.cos(phase)
+    dir_vec = np.array([dx,dy,dz])
+    
+    
+    # define origin
+    # rotate around Y then z to go from XYZ to xyz
+    rotz = np.array([[np.cos(-phase),-np.sin(-phase),0.0],
+                     [np.sin(-phase),np.cos(-phase),0.0],
+                     [0.0,0.0,1.0]])
+    rotY = np.array([[np.cos(-inclination),0.0,np.sin(-inclination)],
+                     [0.0,1.0,0.0],
+                     [-np.sin(-inclination),0.0,np.cos(-inclination)]])
+    origin = np.matmul( np.matmul(np.array([Xstar,Ystar,Zstar]),rotY), rotz) + np.array([xstar,ystar,zstar])
+    print origin
+    
+    
+    # ray 
+    ray={}
+    ray['l'] = np.linspace(0,length,npoints)
+    ray['x'] = np.flipud(ray['l']*dx) + origin[0] 
+    ray['y'] = np.flipud(ray['l']*dy) + origin[1]
+    ray['z'] = np.flipud(ray['l']*dz) + origin[2]
+    
+    # spherical polar
+    ray['phi'],ray['theta'],ray['r'] = cart_to_polar(ray['x'],ray['y'],ray['z'])
+    
+    return ray
