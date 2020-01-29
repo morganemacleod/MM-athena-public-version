ec23803730e94ebe3aecd0d0499fce7fd4d4504d
Morgan MacLeod <morganmacleod@buconero.cfa.harvard.edu>
2020-01-29 10:38:02 -0500

    changes to the read_data function of planet_wind_utils.py

--- analysis/planet_wind_utils.py
+++ analysis/planet_wind_utils.py
@@ -39,13 +39,14 @@ def read_data(fn,orb,
              x1_min=None,x1_max=None,
              x2_min=None,x2_max=None,
              x3_min=None,x3_max=None,
-             gamma=5./3.):
+              gamma=5./3.,
+              pole_dir=2):
     """ Read spherical data and reconstruct cartesian mesh for analysis/plotting """
     
     print "read_data...reading file",fn
     
     
-    d = ar.athdf(fn,level=level,subsample=True,
+    d = ar.athdf(fn,level=level,fast_restrict=True,
                  x1_min=x1_min,x1_max=x1_max,
                  x2_min=x2_min,x2_max=x2_max,
                  x3_min=x3_min,x3_max=x3_max) # approximate arrays by subsampling if level < max
@@ -64,20 +65,9 @@ def read_data(fn,orb,
 
     data_shape = (len(d['x3v']),len(d['x2v']),len(d['x1v']))
    
-       
-    # MAKE grid based coordinates
-    d['gx1v'] = np.zeros(data_shape)
-    for i in range(data_shape[2]):
-        d['gx1v'][:,:,i] = d['x1v'][i]
-    
-    d['gx2v'] = np.zeros(data_shape)
-    for j in range(data_shape[1]):
-        d['gx2v'][:,j,:] = d['x2v'][j]
-
-    d['gx3v'] = np.zeros(data_shape)
-    for k in range(data_shape[0]):
-        d['gx3v'][k,:,:] = d['x3v'][k]
-    
+    d['gx1v']=np.broadcast_to(d['x1v'],(len(d['x3v']),len(d['x2v']),len(d['x1v'])) )
+    d['gx2v']=np.swapaxes(np.broadcast_to(d['x2v'],(len(d['x3v']),len(d['x1v']),len(d['x2v'])) ),1,2)
+    d['gx3v']=np.swapaxes(np.broadcast_to(d['x3v'],(len(d['x1v']),len(d['x2v']),len(d['x3v'])) ) ,0,2 )
     
     ####
     # GET THE VOLUME 
@@ -88,18 +78,10 @@ def read_data(fn,orb,
     d2 = d['x2f'][1:] - d['x2f'][:-1]
     d3 = d['x3f'][1:] - d['x3f'][:-1]
     
-    # grid based versions
-    gd1 = np.zeros(data_shape)
-    for i in range(data_shape[2]):
-        gd1[:,:,i] = d1[i]
+    gd1=np.broadcast_to(d1,(len(d['x3v']),len(d['x2v']),len(d['x1v'])) )
+    gd2=np.swapaxes(np.broadcast_to(d2,(len(d['x3v']),len(d['x1v']),len(d['x2v'])) ),1,2)
+    gd3=np.swapaxes(np.broadcast_to(d3,(len(d['x1v']),len(d['x2v']),len(d['x3v'])) ) ,0,2 )
     
-    gd2 = np.zeros(data_shape)
-    for j in range(data_shape[1]):
-        gd2[:,j,:] = d2[j]
-
-    gd3 = np.zeros(data_shape)
-    for k in range(data_shape[0]):
-        gd3[k,:,:] = d3[k]
     
     # AREA / VOLUME 
     sin_th = np.sin(d['gx2v'])
@@ -122,15 +104,25 @@ def read_data(fn,orb,
         cos_ph = np.cos(d['gx3v']) 
         
         # cartesian coordinates
-        d['x'] = d['gx1v'] * sin_th * cos_ph 
-        d['y'] = d['gx1v'] * sin_th * sin_ph 
-        d['z'] = d['gx1v'] * cos_th
+        if(pole_dir==2):
+            d['x'] = d['gx1v'] * sin_th * cos_ph 
+            d['y'] = d['gx1v'] * sin_th * sin_ph 
+            d['z'] = d['gx1v'] * cos_th
+        if(pole_dir==0):
+            d['y'] = d['gx1v'] * sin_th * cos_ph 
+            d['z'] = d['gx1v'] * sin_th * sin_ph 
+            d['x'] = d['gx1v'] * cos_th
 
-        if(get_cartesian_vel or get_torque or get_energy):
+        if(get_cartesian_vel):
             # cartesian velocities
-            d['vx'] = sin_th*cos_ph*d['vel1'] + cos_th*cos_ph*d['vel2'] - sin_ph*d['vel3'] 
-            d['vy'] = sin_th*sin_ph*d['vel1'] + cos_th*sin_ph*d['vel2'] + cos_ph*d['vel3'] 
-            d['vz'] = cos_th*d['vel1'] - sin_th*d['vel2']  
+            if(pole_dir==2):
+                d['vx'] = sin_th*cos_ph*d['vel1'] + cos_th*cos_ph*d['vel2'] - sin_ph*d['vel3'] 
+                d['vy'] = sin_th*sin_ph*d['vel1'] + cos_th*sin_ph*d['vel2'] + cos_ph*d['vel3'] 
+                d['vz'] = cos_th*d['vel1'] - sin_th*d['vel2']  
+            if(pole_dir==0):
+                d['vy'] = sin_th*cos_ph*d['vel1'] + cos_th*cos_ph*d['vel2'] - sin_ph*d['vel3'] 
+                d['vz'] = sin_th*sin_ph*d['vel1'] + cos_th*sin_ph*d['vel2'] + cos_ph*d['vel3'] 
+                d['vx'] = cos_th*d['vel1'] - sin_th*d['vel2'] 
             
         del cos_th, sin_th, cos_ph, sin_ph
     
@@ -139,7 +131,7 @@ def read_data(fn,orb,
 
 
 def get_midplane_theta(myfile,level=0):
-    dblank=ar.athdf(myfile,level=level,quantities=[],subsample=True)
+    dblank=ar.athdf(myfile,level=level,quantities=[],fast_restrict=True)
 
     # get closest to midplane value
     return dblank['x2v'][ np.argmin(np.abs(dblank['x2v']-np.pi/2.) ) ]
