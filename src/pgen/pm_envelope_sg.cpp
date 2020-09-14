addaf2dfb9a7e386b1e8af7ddec5bea36fe197b2
morganemacleod <morganmacleod@gmail.com>
2020-09-14 08:15:13 -0500

    damping did not work well, introduced oscillations and energy errors, trying no accel on particle 1

--- src/pgen/pm_envelope_sg.cpp
+++ src/pgen/pm_envelope_sg.cpp
@@ -1236,6 +1236,16 @@ void SumGasOnParticleAccels(Mesh *pm, Real (&xi)[3],Real (&ag1i)[3],Real (&ag2i)
     pmb=pmb->next;
   }//end loop over meshblocks
 
+
+  // add a damping to particle 1
+  if(damp_particle_1==true){
+    for (int ii = 0; ii < 3; ii++){
+      ag1i[ii] = 0.0; //-= vcom_star[ii] / tau_damp_particle_1;
+    }
+  }
+  //std::cout<<"accels:"<<vcom[0]<<" "<<vcom_star[0]<<"\n";
+
+
 #ifdef MPI_PARALLEL
   // sum over all ranks
   if (Globals::my_rank == 0) {
@@ -1250,13 +1260,6 @@ void SumGasOnParticleAccels(Mesh *pm, Real (&xi)[3],Real (&ag1i)[3],Real (&ag2i)
   MPI_Bcast(ag1i,3,MPI_ATHENA_REAL,0,MPI_COMM_WORLD);
   MPI_Bcast(ag2i,3,MPI_ATHENA_REAL,0,MPI_COMM_WORLD);
 #endif
-  // add a damping to particle 1
-  if(damp_particle_1==true){
-    for (int ii = 0; ii < 3; ii++){
-      ag1i[ii] -= vcom_star[ii] / tau_damp_particle_1;
-    }
-  }
-  std::cout<<"accels:"<<vcom[0]<<" "<<vcom_star[0]<<"\n";
     
 }
 
